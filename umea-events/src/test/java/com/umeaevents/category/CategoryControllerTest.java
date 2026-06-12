37b6ea7bfcabd0dc8493c49b3829c1faa9c1e95b
Ulfharry <ulfharryalexandersson@gmail.com>
2026-06-12 21:23:12 +0200

    Update CategoryControllerTest.java

--- umea-events/src/test/java/com/umeaevents/category/CategoryControllerTest.java
+++ umea-events/src/test/java/com/umeaevents/category/CategoryControllerTest.java
@@ -4,7 +4,7 @@ import com.umeaevents.category.dto.CategoryResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -26,7 +26,7 @@ class CategoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private CategoryService categoryService;
 
     @Test
